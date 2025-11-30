7f443ae1f88aaa1bede179308b0961a93a050efd
belato <sola.belator@gmail.com>
2025-11-30 22:45:50 +0300

    update style & Main.jsx

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,14 +1,30 @@
+import axios from "axios"
+import routes from "../routes"
+import { useNavigate } from "react-router-dom"
+import { useEffect } from "react"
+
+const getAuthHeader = () => {
+  const userId = JSON.parse(localStorage.getItem('userId'))
+
+  if (userId && userId.token) {
+    return { Authorization: `Bearer ${userId.token}` }
+  }
+  return {}
+}
 
 export const Main = () => {
+  const navigate = useNavigate()
+  const header = getAuthHeader()
+
+  useEffect(() => {
+    if (!header.Authorization) {
+      navigate('/login')
+    }
+  }, [])
   
   return (
     <>
       <div className="container-fluid m-0">
-        <div className="navbar bg-body-tertiary bg-secondary-subtle container-fluid m-0">
-          <div className="container-fluid ps-5">
-            <a href="/" className="navbar-brand ps-5 mb-0 h1">Join Chat</a>
-          </div>
-        </div>
         <div className="row justify-content-center align-items-center h-100">
           <div className="col-4">
             <div className="card mt-2">
