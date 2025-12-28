e10b189ea42974d6efabe36de3b0bcb6fb0e73f0
belato <sola.belator@gmail.com>
2025-12-28 17:24:18 +0300

    add toast hook

---
+++ frontend/src/hooks/useToast.js
@@ -0,0 +1,30 @@
+import { toast } from 'react-toastify'
+
+export const useToast = () => {
+  const toastSuccess = (message) => {
+    toast.success(message, {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    })
+  }
+
+  const toastError = (message) => {
+    toast.error(message, {
+      position: "top-right",
+      autoClose: 4000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: 0.5,
+    })
+  }
+
+  return { toastError, toastSuccess, toast }
+}
+
